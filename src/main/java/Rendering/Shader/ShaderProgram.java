0d43d84c64042cd0a1c358636fcc824ed4b45393
Eric Lavesson <eric.lavesson@gmail.com>
2016-06-25 23:50:25 +0200

    Create a class for shader programs

---
+++ src/main/java/Rendering/Shader/ShaderProgram.java
@@ -0,0 +1,58 @@
+package Rendering.Shader;
+import com.sun.media.jfxmediaimpl.MediaDisposer;
+
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL20.*;
+
+public class ShaderProgram implements AutoCloseable {
+    private final int program;
+
+    public ShaderProgram(String vertexSource, String fragmentSource) throws ShaderException {
+        // Create a new shader program
+        program = glCreateProgram();
+
+        // Create and compile each shader and link them together into a program
+        link(compile(glCreateShader(GL_VERTEX_SHADER), vertexSource),
+             compile(glCreateShader(GL_FRAGMENT_SHADER), fragmentSource));
+    }
+
+    public void bind() {
+        glUseProgram(program);
+    }
+
+    // Attaches the vertex and fragment shaders to the program and links it
+    private void link(int vert, int frag) throws ShaderException {
+        // Attach the shaders
+        glAttachShader(program, vert);
+        glAttachShader(program, frag);
+
+        // Link the program
+        glLinkProgram(program);
+
+        // Detach and delete the shaders - we're done with them and can use the program from now on
+        glDetachShader(program, vert);
+        glDetachShader(program, frag);
+
+        if (glGetShaderi(program, GL_LINK_STATUS) == GL_FALSE) {
+            throw new ShaderException("Could not link shader program", glGetProgramInfoLog(program));
+        }
+    }
+
+    // Compile a shader (used regardless of whether we're dealing with a fragment or a vertex shader)
+    private int compile(int shader, String sourceCode) throws ShaderException {
+        glShaderSource(shader, sourceCode);
+        glCompileShader(shader);
+
+        if (glGetShaderi(shader, GL_COMPILE_STATUS) == GL_FALSE) {
+            throw new ShaderException("Could not compile shader", glGetShaderInfoLog(shader));
+        }
+
+        return shader;
+    }
+
+    @Override
+    public void close() throws Exception {
+        // Delete the program when closing
+        glDeleteProgram(program);
+    }
+}
