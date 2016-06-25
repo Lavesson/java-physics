95fe5cba39a330c4551b23b0cca33414ff5d78c7
Eric Lavesson <eric.lavesson@gmail.com>
2016-06-25 17:03:36 +0200

    Create an empty rendering window

---
+++ src/main/java/Rendering/LWJGL3Renderer.java
@@ -0,0 +1,78 @@
+package Rendering;
+
+import org.lwjgl.*;
+import org.lwjgl.glfw.*;
+import org.lwjgl.opengl.*;
+
+import static org.lwjgl.glfw.Callbacks.*;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryUtil.*;
+
+public class LWJGL3Renderer {
+    private final int width;
+    private final int height;
+    private long window;
+
+    public LWJGL3Renderer(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public void run() {
+        try {
+            init();
+            loop();
+
+            glfwFreeCallbacks(window);
+            glfwDestroyWindow(window);
+        } finally {
+            glfwTerminate();
+            glfwSetErrorCallback(null).free();
+        }
+    }
+
+    private void loop() {
+        GL.createCapabilities();
+        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+        while ( !glfwWindowShouldClose(window) ) {
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+            glfwSwapBuffers(window);
+            glfwPollEvents();
+        }
+    }
+
+    private void init() {
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        if ( !glfwInit() )
+            throw new IllegalStateException("Unable to initialize GLFW");
+
+        // Window configuration
+        glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+
+        window = glfwCreateWindow(width, height, "Physics Demo", NULL, NULL);
+
+        if ( window == NULL )
+            throw new RuntimeException("Failed to create the GLFW window");
+
+        glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
+            if ( key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE )
+                glfwSetWindowShouldClose(window, true); // We will detect this in our rendering loop
+        });
+
+        GLFWVidMode mode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+        glfwSetWindowPos(
+                window,
+                (mode.width() - width) / 2,
+                (mode.height() - height) / 2
+        );
+
+        glfwMakeContextCurrent(window);
+        glfwSwapInterval(1);
+        glfwShowWindow(window);
+    }
+}
