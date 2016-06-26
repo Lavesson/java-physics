b3fb27a38bf0bd8fe1b4239b55daab1a58409b69
Eric Lavesson <eric.lavesson@gmail.com>
2016-06-26 19:40:26 +0200

    Fix a constraint issue
    There's still some issues with the constraint solver, but this is
    still way better than before.

--- src/main/java/Physics/EdgeSeparation.java
+++ src/main/java/Physics/EdgeSeparation.java
@@ -17,13 +17,19 @@ public class EdgeSeparation {
 
         // Right edge (r1) to left edge (r2). Pick the smallest between the previous calculation and our new one
         // The minus sign in the second part is to make sure we translate in the correct direction (left)
-        results.x = Math.min(results.x, -Math.abs(r1.x + r1.width - r2.x));
+        float rightToLeft = Math.abs(r1.x + r1.width - r2.x);
+        results.x = Float.compare(Math.min(results.x, rightToLeft), rightToLeft) == 0
+                ? -rightToLeft
+                : results.x;
 
         // Top edge (r1) to bottom edge (r2)
         results.y = Math.abs(r1.y - (r2.y + r2.height));
 
         // Bottom edge (r1) to top edge (r2) - same deal as for the second case in x
-        results.y = Math.min(results.y, -Math.abs(r1.y+r1.height-r2.y));
+        float bottomToTop = Math.abs(r1.y + r1.height - r2.y);
+        results.y = Float.compare(Math.min(results.y, bottomToTop), bottomToTop) == 0
+                ? -bottomToTop
+                : results.y;
 
         return results;
     }
